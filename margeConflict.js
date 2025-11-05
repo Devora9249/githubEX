763cd086408e3b1bb52b165ee63f09f4040f976e
Devora Wexler <devora.video@gmail.com>
2025-11-05 20:26:51 +0200

    change in branch feature-1

---
+++ margeConflict.js
@@ -0,0 +1,12 @@
+const func1 = ()=>{
+    console.log("Function 1");
+}
+
+const func2 = ()=>{
+    console.log("Function 2");
+}
+
+const func3 = ()=>{
+    console.log("Function 3 was changed!");
+    console.log("Additional line in func3");
+}
